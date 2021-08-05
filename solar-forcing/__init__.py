c263aa22a5a816d4643677ee8fb7d0c80a78504c
Max Grover <mgroverwx@gmail.com>
2021-08-05 15:06:22 -0600

    create package

---
+++ solar-forcing/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" Top-level module for solar-forcing. """
+from pkg_resources import DistributionNotFound, get_distribution
+from .calc import  gen_energy_grid, vdk2016, calculate_flux, lshell_to_glat, glat_to_lshell
+from .data_access import grab_potsdam_file
+from .main import FluxCalculation
